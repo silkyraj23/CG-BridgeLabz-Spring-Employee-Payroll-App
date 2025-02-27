5cb23a3882d2f057f3e61f35f98da27ef355a5ea
Shamiksha <silkyraj23@gmail.com>
2025-02-28 01:11:01 +0530

    [shamiksha] add: Repository created ✨

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/repositories/EmployeeRepository.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/repositories/EmployeeRepository.java
@@ -4,7 +4,7 @@ package com.bridgelabz.employeepayrollapp.repositories;
 import com.bridgelabz.employeepayrollapp.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//Repository used to store data in a Database
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 }
